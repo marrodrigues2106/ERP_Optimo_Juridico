b8a9e76b4a29abd9e863fb068efc83913a91ea50
Skip <skip@usecurling.com>
2026-03-21 03:04:19 +0000

    Fix admin login and auth error

--- pocketbase/migrations/0007_fix_admin_credentials.js
+++ pocketbase/migrations/0007_fix_admin_credentials.js
@@ -2,20 +2,6 @@ migrate(
   (app) => {
     const users = app.findCollectionByNameOrId('users')
 
-    // Update password field to allow minimum of 4 characters, required for setting it to '1234'
-    try {
-      const pwdField = users.fields.getByName('password')
-      if (pwdField) {
-        pwdField.min = 4
-        app.save(users)
-      } else if (users.authOptions) {
-        users.authOptions.minPasswordLength = 4
-        app.save(users)
-      }
-    } catch (e) {
-      console.log('Failed to update password min length:', e)
-    }
-
     let admin
     try {
       admin = app.findAuthRecordByEmail('users', 'mmr.juridico@gmail.com')
@@ -24,7 +10,7 @@ migrate(
       admin.setEmail('mmr.juridico@gmail.com')
     }
 
-    admin.setPassword('1234')
+    admin.setPassword('securepassword123')
     admin.set('isAdmin', true)
     admin.setVerified(true)
 
